56cf803c0b40e48b05139a0bf56f6fb1656a3a44
Nadia Khoptiuk <holineinadia@gmail.com>
2022-09-02 10:48:08 +0300

    fix validate

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -3,17 +3,17 @@ function validate(schema) {
     const result = schema.validate(req.body);
 
     if (result.error) {
-      if (result.error.details[0].context.key) {
-        const errorField = result.error.details[0].context.key;
+      console.log("result.error >>>", result.error.details[0].context);
 
-        return res
-          .status(400)
-          .json({ message: `missing value or error at ${errorField} field` });
+      const errorField = result.error.details[0].context.key;
+
+      if (errorField === "favorite") {
+        return res.status(400).json({ message: "missing field favorite" });
       }
 
       return res
         .status(400)
-        .json({ message: "no one field exists in the body" });
+        .json({ message: `missing value or error at ${errorField} field` });
     }
 
     req.body = result.value;
